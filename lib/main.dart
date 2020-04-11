75cf3ff97de68a78a46698ae7799797bf312fbb4
MichaelMohn <50528867+MichaelMohn@users.noreply.github.com>
2020-04-11 17:59:49 -0500

    login and register update

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,6 @@ class LoginPage extends StatefulWidget {
 }
 
 class _pageState extends State<LoginPage> {
-
   String name;
   String password;
 
@@ -30,59 +29,96 @@ class _pageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: ListView(
-        children: <Widget>[
-          Row(
-            children: <Widget>[
-              Expanded(
-                  child: Column(children: <Widget>[
-
-                    TextField(
-                        controller: nameController,
-                        decoration: InputDecoration(
-                            labelStyle: TextStyle(fontSize: 24),
-                            border: InputBorder.none,
-                            hintText: 'username',
-                            labelText: "Enter your username"),
-                        onChanged: (text) {
-                          print("Second text field: $text");
-
-                          setState(() {
+      backgroundColor: Colors.green,
+      body: ListView(children: <Widget>[
+        Column(
+          children: <Widget>[
+            SizedBox(height: 100),
+            Text("EcoCycle"),
+            SizedBox(height: 260),
+            Container(
+                margin:
+                    const EdgeInsets.only(left: 30.0, top: 60.0, right: 30.0),
+                height: 170.0,
+                decoration: new BoxDecoration(
+                    color: Colors.white,
+                    borderRadius:
+                        new BorderRadius.all(new Radius.circular(25.7))),
+                child: new Directionality(
+                    textDirection: TextDirection.ltr,
+                    child: Column(
+                      children: <Widget>[
+                        SizedBox(height: 10),
+                        Text('Login'),
+                        new TextField(
+                          controller: null,
+                          autofocus: false,
+                          style: new TextStyle(
+                              fontSize: 22.0, color: Color(0xFFbdc6cf)),
+                          decoration: new InputDecoration(
+                            filled: true,
+                            fillColor: Colors.white,
+                            hintText: 'Username',
+                            contentPadding: const EdgeInsets.only(
+                                left: 14.0, bottom: 8.0, top: 8.0),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: new BorderSide(color: Colors.white),
+                              borderRadius: new BorderRadius.circular(25.7),
+                            ),
+                            enabledBorder: UnderlineInputBorder(
+                              borderSide: new BorderSide(color: Colors.white),
+                              borderRadius: new BorderRadius.circular(25.7),
+                            ),
+                          ),
+                          onChanged: (text) {
                             name = text;
-                          });
-                        }),
-                    //Name
-
-                    //password
-                    TextField(
-                        controller: passwordController,
-                        decoration: InputDecoration(
-                            labelStyle: TextStyle(fontSize: 24),
-                            border: InputBorder.none,
-                            hintText: 'password',
-                            labelText: "Enter your password"),
-                        onChanged: (text) {
-                          print("Second text field: $text");
-
-                          setState(() {
+                          },
+                        ),
+                        new TextField(
+                          controller: null,
+                          autofocus: false,
+                          style: new TextStyle(
+                              fontSize: 22.0, color: Color(0xFFbdc6cf)),
+                          decoration: new InputDecoration(
+                            filled: true,
+                            fillColor: Colors.white,
+                            hintText: 'Password',
+                            contentPadding: const EdgeInsets.only(
+                                left: 14.0, bottom: 8.0, top: 8.0),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: new BorderSide(color: Colors.white),
+                              borderRadius: new BorderRadius.circular(25.7),
+                            ),
+                            enabledBorder: UnderlineInputBorder(
+                              borderSide: new BorderSide(color: Colors.white),
+                              borderRadius: new BorderRadius.circular(25.7),
+                            ),
+                          ),
+                          onChanged: (text) {
                             password = text;
-                          });
-                        }),
-                    FlatButton(
-                      child: Text('register'),
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => Register()));
-                      },
-
-                    )
-              ]))
-            ],
-          )
-        ],
-      ),
+                          },
+                        ),
+                        FlatButton(
+                          child: Text(
+                              'Submit'
+                          ),
+                          onPressed: () {},
+                        ),
+                      ],
+                    ))),
+            RaisedButton(
+              child: Text(
+                  "Brand new? Let's register!"
+              ),
+              onPressed: (){
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Register()));
+              },
+            )
+          ],
+        )
+      ]),
     );
   }
 }
