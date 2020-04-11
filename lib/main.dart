d0963d47dc10f9a2703bbce4fc169d96222dde08
Samuel Yuan <samuel6161@gmail.com>
2020-04-11 18:01:12 -0500

    homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
@@ -52,20 +51,13 @@ class _MyHomePageState extends State<MyHomePage> {
             ? Text('No image selected.')
             : Image.file(_image),
       ),
-        floatingActionButton: Column(
-          children: <Widget>[
+        floatingActionButton:
             FloatingActionButton(
               onPressed: getImage,
               tooltip: 'Pick Image',
               child: Icon(Icons.add_a_photo),
             ),
-            FloatingActionButton(
-              onPressed: getImageFile,
-              tooltip: 'Pick Image',
-              child: Icon(Icons.add_photo_alternate),
-            ),
-          ],
-        )
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
     );
   }
 }
