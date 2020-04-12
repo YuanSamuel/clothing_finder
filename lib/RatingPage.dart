d19cdd01b5b550069e0acbd9bfe99441a3347eaf
Samuel Yuan <samuel6161@gmail.com>
2020-04-12 02:25:31 -0500

    voting and points

--- lib/RatingPage.dart
+++ lib/RatingPage.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'dart:ui';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'models/entry.dart';
 
 class RatingPage extends StatefulWidget {
   RatingPage({Key key, this.posts}) : super(key: key);
@@ -12,89 +13,111 @@ class RatingPage extends StatefulWidget {
 }
 
 class _RatingPageState extends State<RatingPage> {
+
+  double stars;
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<DocumentSnapshot>(
-    future: getUserPosts(),
-    builder: (context, snapshot) {
-      if (snapshot.connectionState != ConnectionState.done) {
-        return Scaffold(
-          body: CircularProgressIndicator(),
-        );
-      }
-      else {
-        widget.posts.sort((a, b) {
-          return a.data['votes'].compareTo(b.data['votes']);
-        });
-        for (int i = 0; i < widget.posts.length; i++) {
-          if (widget.posts[i].data['userId'] != snapshot.data.data['uid'] && snapshot.data.data['rated'].contains(widget.posts[i].documentID)) {
+        future: getUserPosts(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState != ConnectionState.done) {
             return Scaffold(
-              appBar: AppBar(
-                  centerTitle: true,
-                  title: Text(
-                    'Rating Page',
-                    style: TextStyle(
-                        fontStyle: FontStyle.italic
-                    ),
-                  )
-              ),
-              body: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Column(
-                    children: <Widget>[
-                      Padding(
-                        padding: EdgeInsets.fromLTRB(32, 32, 32, 12),
-                        child: Container(
-                          child: Image.network(widget.posts[i].data['url']),
-                          height: MediaQuery.of(context).size.height / 1.2,
-                          width: MediaQuery.of(context).size.width / 1.2,
-                        ),
-                      ),
-                      RatingBar(
-                        initialRating: 3,
-                        minRating: 1,
-                        direction: Axis.horizontal,
-                        itemCount: 5,
-                        itemSize: 32,
-                        itemPadding: EdgeInsets.symmetric(horizontal: 2.0),
-                        itemBuilder: (context, _) => Icon(
-                          Icons.star,
-                          color: Colors.amber,
-                        ),
-                        onRatingUpdate: (rating) {
-                          print(rating);
-                        },
-                      ),
-                      SizedBox(height: 6),
-                      ButtonTheme(
-                        minWidth: 200,
-                        height: 40,
-                        child: RaisedButton(
-                          child: Text(
-                            'Confirm Rating',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 22
+              body: CircularProgressIndicator(),
+            );
+          } else {
+            widget.posts.sort((a, b) {
+              return a.data['votes'].compareTo(b.data['votes']);
+            });
+            for (int i = 0; i < widget.posts.length; i++) {
+              entry post = entry.fromSnapshot(widget.posts[i]);
+              if (post.userId != snapshot.data.data['uid'] &&
+                  !snapshot.data.data['rated']
+                      .contains(widget.posts[i].documentID)) {
+                print('return');
+                return Scaffold(
+                  appBar: AppBar(
+                      centerTitle: true,
+                      title: Text(
+                        'Rating Page',
+                        style: TextStyle(fontStyle: FontStyle.italic),
+                      )),
+                  body: SizedBox.expand(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(12, 20, 12, 20),
+                          child: Container(
+                            child: Image.network(
+                             post.url,
+                              height: MediaQuery.of(context).size.height / 1.5,
+                              width: MediaQuery.of(context).size.width / 1.2,
                             ),
                           ),
-                          color: Colors.amber,
-                          onPressed: () {},
                         ),
-                      )
-                    ],
+                        RatingBar(
+                          initialRating: 3,
+                          minRating: 1,
+                          direction: Axis.horizontal,
+                          itemCount: 5,
+                          itemSize: 32,
+                          itemPadding: EdgeInsets.symmetric(horizontal: 2.0),
+                          itemBuilder: (context, _) => Icon(
+                            Icons.star,
+                            color: Colors.amber,
+                          ),
+                          onRatingUpdate: (rating) {
+                            stars = rating;
+                            print(rating);
+                          },
+                        ),
+                        SizedBox(height: 6),
+                        ButtonTheme(
+                          minWidth: 200,
+                          height: 40,
+                          child: RaisedButton(
+                            child: Text(
+                              'Confirm Rating',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 22),
+                            ),
+                            color: Colors.amber,
+                            onPressed: () {
+                              Navigator.pop(context);
+                              updatePoints(snapshot.data, post);
+                            },
+                          ),
+                        )
+                      ],
+                    ),
                   ),
-                ],
-              ),
-            );
+                );
+              }
+            }
+            return Scaffold(
+                body: Center(
+              child: Text('Nothing to vote for!'),
+            ));
           }
-        }
-        return Scaffold(
-          body: Center(
-            child: Text('Nothing to vote for!'),
-          )
-        );
-      }
+        });
+  }
+
+  updatePoints(DocumentSnapshot currentUserData, entry post) async {
+    List rated = currentUserData.data['rated'];
+    rated.add(post.reference.documentID);
+    currentUserData.reference.updateData({
+      'points': currentUserData.data['points'] + 1,
+      'ratings': currentUserData.data['ratings'] + 1,
+      'rated': rated,
+    });
+    DocumentSnapshot otherUser = await Firestore.instance.collection('users').document(post.userId).get();
+    Firestore.instance.collection('users').document(post.userId).updateData({
+      'points': otherUser.data['points'] + (stars - 2),
+    });
+    Firestore.instance.collection('posts').document(post.reference.documentID).updateData({
+      'rating': (post.rating * post.votes + stars) / (post.votes + 1),
+      'votes': post.votes + 1,
     });
   }
 
--- lib/leaderboard.dart
+++ lib/leaderboard.dart
@@ -1,31 +1,32 @@
+import 'dart:math';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class LeaderBoard extends StatelessWidget {
-
   _space(int place, String name, int star) {
     return Row(
       children: [
-        if(place==1)
+        if (place == 1)
           Image.asset(
             'assets/1st.png',
             height: 25,
           ),
-        if(place==2)
+        if (place == 2)
           Image.asset(
             'assets/2nd.png',
             height: 25,
           ),
-        if(place==3)
-          Image.asset(''
+        if (place == 3)
+          Image.asset(
+            ''
             'assets/3rd.png',
             height: 25,
           ),
-        if(place>3)
+        if (place > 3)
           Text(
             place.toString(),
-            style: TextStyle(
-              fontSize: 25
-            ),
+            style: TextStyle(fontSize: 25),
           ),
         Container(
           width: 12,
@@ -64,83 +65,97 @@ class LeaderBoard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView(
-        children: [
-          Container(
-            height: 250,
-            child: Stack(
-              children: [
-                Image.asset(
-                  'assets/confetti.gif',
-                  width: MediaQuery.of(context).size.width,
-                ),
-                Container(
-                  alignment: Alignment.bottomCenter,
-                  child: Row(
-                    children: [
-                      Spacer(
-                        flex: 1,
-                      ),
-                      Column(
-                        children: [
-                          Container(
-                            height: 150,
+    return FutureBuilder<QuerySnapshot>(
+        future: Firestore.instance.collection('users').getDocuments(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState != ConnectionState.done) {
+            return Scaffold(
+              body: Center(
+                child: Text('Loading'),
+              ),
+            );
+          } else {
+            List<DocumentSnapshot> docs = snapshot.data.documents;
+            docs.sort((user1, user2) {
+              return user2.data['points'].compareTo(user1.data['points']);
+            });
+            return Scaffold(
+              body: ListView(
+                children: [
+                  Container(
+                    height: 250,
+                    child: Stack(
+                      children: [
+                        Image.asset(
+                          'assets/confetti.gif',
+                          width: MediaQuery.of(context).size.width,
+                        ),
+                        Container(
+                          alignment: Alignment.bottomCenter,
+                          child: Row(
+                            children: [
+                              Spacer(
+                                flex: 1,
+                              ),
+                              Column(children: [
+                                Container(
+                                  height: 150,
+                                ),
+                                Container(
+                                  color: Colors.grey,
+                                  width: 75,
+                                  height: 100,
+                                ),
+                              ]),
+                              Column(
+                                children: [
+                                  Container(
+                                    height: 100,
+                                  ),
+                                  Container(
+                                    color: Colors.yellow,
+                                    width: 75,
+                                    height: 150,
+                                  ),
+                                ],
+                              ),
+                              Column(
+                                children: [
+                                  Container(
+                                    height: 200,
+                                  ),
+                                  Container(
+                                    color: Colors.brown,
+                                    width: 75,
+                                    height: 50,
+                                  ),
+                                ],
+                              ),
+                              Spacer(
+                                flex: 1,
+                              ),
+                            ],
                           ),
-                          Container(
-                            color: Colors.grey,
-                            width: 75,
-                            height: 100,
-                          ),
-                        ],
-                      ),
-                      Column(
-                        children: [
-                          Container(
-                            height: 100,
-                          ),
-                          Container(
-                            color: Colors.yellow,
-                            width: 75,
-                            height: 150,
-                          ),
-                        ],
-                      ),
-                      Column(
-                        children: [
-                          Container(
-                            height: 200,
-                          ),
-                          Container(
-                            color: Colors.brown,
-                            width: 75,
-                            height: 50,
-                          ),
-                        ],
-                      ),
-                      Spacer(
-                        flex: 1,
-                      ),
-                    ],
+                        )
+                      ],
+                    ),
                   ),
-                )
-              ],
-            ),
-          ),
-          Container(
-            child: Column(
-              children: [
-                for(int i=1;i<11;i++)
-                  Padding(
-                    padding: EdgeInsets.all(10),
-                    child: _space(i, 'Person${i}', 10),
-                  ),
-              ],
-            ),
-          )
-        ],
-        scrollDirection: Axis.vertical,
-      ),
-    );
+                  Container(
+                    child: Column(
+                      children: [
+                        for (int i = 0; i < min(docs.length, 10); i++)
+                          Padding(
+                            padding: EdgeInsets.all(10),
+                            child: _space(i + 1, docs[i].data['name'], docs[i].data['points'].round()),
+                          ),
+                      ],
+                    ),
+                  )
+                ],
+                scrollDirection: Axis.vertical,
+              ),
+            );
+          }
+        });
   }
 }
